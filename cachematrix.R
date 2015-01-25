2b001e355d2ed460bb97692fc66a425e449b035d
NailaKarim.pk <nailakarim.pk@gmail.com>
2015-01-25 19:50:37 +0500

    Code For Assignment 02 is added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
+## This file contains the code for R Programming course: Assignment02
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates list containing a function 
+## set the value of the Matrix
+## get the value of the Matrix
+## set the inverse of the Matrix
+## get the inverse of the Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+#Reset inverse 
+inverse <- NULL
+set <- function(y) {
+# Reset values of m and inverse in parent environment 
+m <<- y
+inverse <<- NULL
+}
+get <- function() m
+setinverse <- function(i) inverse <<-i # Reset values of inverse in parent environment 
+getinverse <- function() inverse
+#Create and return an anonymous list made of four components
+list(set=set, get=get, setinverse=setinverse, getinverse=getinverse )
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve calculates the inverse of the special "vector" created with the makeCacheMatrix function. 
+## It calculates inverse only if the matrix is invertible. It is ensured using condition determinant !=0
+## Only if it is not already created. For this purpose it first checks the cached inverse, to avoid re-computation. 
+
+cacheSolve <-function(matrixList, ...) {
+        
+		#check if Inverse is already computed
+        inverse <- matrixList$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+		#get the matrix created using makeCacheMatrix
+        temp <- matrixList$get()
+		#Check if matrix is invertible
+		if(det(temp)!=0) {
+		inverse <- solve(temp)
+		#Compute inverse of matrix
+		matrixList$setinverse(inverse)
+        return(inverse)
+		}
+		else
+		{
+			message("It is not an Invertible Matrix")  
+			return(inverse)
+		}		
+        
 }
